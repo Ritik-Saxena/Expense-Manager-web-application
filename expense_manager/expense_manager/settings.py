f0cf2f732de5f759763828e8070d0d12c655301f
Ritik Saxena <62079355+Ritik-Saxena@users.noreply.github.com>
2023-12-03 16:15:15 +0530

    Update settings.py

--- expense_manager/expense_manager/settings.py
+++ expense_manager/expense_manager/settings.py
@@ -149,8 +149,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'djangotestofficial@gmail.com'
-EMAIL_HOST_PASSWORD = 'wogh zugb jycm xgyb'
+EMAIL_HOST_USER = 'demo_mail@xyz.com'
+EMAIL_HOST_PASSWORD = 'demo_password'
 
 
 
